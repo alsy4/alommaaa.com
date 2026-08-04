0e59fcc49999ba2878958b4eb5f397901a260a95
alsy4 <syahiralimi@gmail.com>
2026-08-04 18:45:59 +0800

    Updated GITHUB token instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alome.me",
+  site: "https://alommaaa.com",
   integrations: [mdx(), sitemap()],
   markdown: {
     processor: unified({
